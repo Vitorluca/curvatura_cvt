22c4cc04e340023468a37a69240bcd4c139f642e
Vitorluca <vitor8391244603@gmail.com>
2024-11-08 21:48:11 -0300

    adiciona a função de salvar os dados do rpm

--- rpm_acquisition/src/main.cpp
+++ rpm_acquisition/src/main.cpp
@@ -1,5 +1,10 @@
 #include <Arduino.h>
 
+//ibrares for SD card
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
 int32_t pin_rpm = 36;
 int rpm = 0;
 int pulse_count = 0;
@@ -15,15 +20,62 @@ int data_rpm() {
   return rpm;
 }
 
+// Function to write file in sd card
+void writeFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)){
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(pin_rpm, INPUT);
-  attachInterrupt(pin_rpm, funcao_ISR, RISING);  
+  attachInterrupt(pin_rpm, funcao_ISR, RISING);
+
+  if(!SD.begin(5)){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  Serial.print("SD Card Type: ");
+  if(cardType == CARD_MMC){
+    Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+    Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
 }
 
 void loop() {
   data_rpm();
+  Serial.print("RPM: ");
   Serial.println(rpm);
+
+  writeFile(SD, "/data_log.csv", "Hello\n"); // Write file in sd card
+
   delay(100);
 }
 
